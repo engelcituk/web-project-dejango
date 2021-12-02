419ac47d3c04f9e18faf669a4157c67f9e733304
cituk <programaciondyn3.mx@sandos.com>
2021-12-01 23:17:23 -0500

    Templates en Django

--- webproject/views.py
+++ webproject/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home( request ):
-    return HttpResponse('<h2>Bienvenidos</h2>')
+    # return HttpResponse('<h2>Bienvenidos</h2>')
+    return render( request, 'home.html')
